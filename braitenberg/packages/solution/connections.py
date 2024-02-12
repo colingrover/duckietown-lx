a09f8e04c4242e0affa1448968af05fedadd57f9
colingrover <102879108+colingrover@users.noreply.github.com>
2024-02-12 16:07:04 -0500

    feb 12 commit 1

--- braitenberg/packages/solution/connections.py
+++ braitenberg/packages/solution/connections.py
@@ -4,12 +4,11 @@ import numpy as np
 
 
 def get_motor_left_matrix(shape: Tuple[int, int]) -> np.ndarray:
-    # TODO: write your function instead of this one
     res = np.zeros(shape=shape, dtype="float32")
-    # these are random values
-    res[0:300, 250:] = 1
-    res[301:, 250:] = -1
-    # ---
+
+    res[250:, :shape[1]//2] = 1
+    res[250:, (shape[1]//2) + 1:] = -1
+    
     return res
 
 
