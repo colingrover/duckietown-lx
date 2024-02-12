b6878ee6edb96c182c288e35c3d028f815802669
colingrover <102879108+colingrover@users.noreply.github.com>
2024-02-12 17:19:57 -0500

    end of feb 12 lab

--- braitenberg/packages/solution/connections.py
+++ braitenberg/packages/solution/connections.py
@@ -6,8 +6,9 @@ import numpy as np
 def get_motor_left_matrix(shape: Tuple[int, int]) -> np.ndarray:
     res = np.zeros(shape=shape, dtype="float32")
 
-    res[250:, :shape[1]//2] = 1
-    res[250:, (shape[1]//2) + 1:] = -1
+    res[200:400, :(shape[1] // 2)] = 1
+    res[200:400, ((shape[1] // 2) + 1):] = -1
+    # res[2*shape[0]//3:, ((shape[1] // 2) + 1):] = -1
     
     return res
 
