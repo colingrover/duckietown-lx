9cad075b5cf6fcdba48b3bfe8a9037998c4af7ba
colingrover <102879108+colingrover@users.noreply.github.com>
2024-02-17 20:03:54 -0500

    This shit is NOT working

--- braitenberg/packages/solution/connections.py
+++ braitenberg/packages/solution/connections.py
@@ -1,13 +1,19 @@
 from typing import Tuple
-
+import math
 import numpy as np
 
 
 def get_motor_left_matrix(shape: Tuple[int, int]) -> np.ndarray:
     res = np.zeros(shape=shape, dtype="float32")
 
-    res[200:400, :(shape[1] // 2)] = 1
-    res[200:400, ((shape[1] // 2) + 1):] = -1
+    full = shape[0]
+
+    res[full:, :(shape[1] // 2)] = 1
+    res[full:, ((shape[1] // 2) + 1):] = -1
+
+    for i in range(200, full):
+        res[i, (full-i):(shape[1] // 2)] = 1
+        res[i, ((shape[1] // 2) + 1):(shape[1] - (full-i))] = -1
     # res[2*shape[0]//3:, ((shape[1] // 2) + 1):] = -1
     
     return res
--- braitenberg/packages/solution/preprocessing.py
+++ braitenberg/packages/solution/preprocessing.py
@@ -1,8 +1,8 @@
 import cv2
 import numpy as np
 
-lower_hsv = np.array([0, 5, 50])
-upper_hsv = np.array([30, 175, 255])
+lower_hsv = np.array([0, 100, 100])
+upper_hsv = np.array([30, 255, 255])
 
 
 def preprocess(image_rgb: np.ndarray) -> np.ndarray:
